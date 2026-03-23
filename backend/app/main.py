c60350aeb3196cd1ed6b348eb01b27356502e3a2
Rashmi Ranjan Badajena <rashmiranjanbadajena.it@gmail.com>
2026-03-23 22:18:01 +0530

    Add new CORS origin for production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,8 @@ async def startup_event():
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["http://localhost:3000"
+                  "https://learnoir-ai.vercel.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
